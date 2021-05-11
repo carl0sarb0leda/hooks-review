d9dc49c7dc2a4620b6d65148972b9c08c5c098b6
carl0sarb0leda <carlosaepn@gmail.com>
2021-05-11 22:23:18 +1200

    Update types for ThemeContext and logic for value in Provider

--- src/context/themeContext.tsx
+++ src/context/themeContext.tsx
@@ -1,15 +1,17 @@
 import React, { ReactNode, useState } from 'react';
 
-// type State = string
-// type Dispatch = () => void
+// Defining types
+type State = string | undefined
+type SetState = React.Dispatch<React.SetStateAction<State>>
+type ThemeContextProps = { color: State, setColor: SetState } | undefined
 type ThemeProviderProps = { children: ReactNode }
 
-const ThemeContext = React.createContext<any>(undefined);
+const ThemeContext = React.createContext<ThemeContextProps>(undefined);
 
 // Custom Provider component
 const ThemeProvider = ({ children }: ThemeProviderProps) => {
     const [color, setColor] = useState<string>()
-    const value = React.useMemo(() => [color, setColor], [color])
+    const value = React.useMemo(() => { return { color, setColor } }, [color])
     return (
         <ThemeContext.Provider value={value}>
             {children}
@@ -22,7 +24,7 @@ const useTheme = () => {
     if (!context) {
         throw new Error(`useTheme must be used with a ThemeProvider`)
     }
-    const [color, setColor] = context
+    const { color, setColor } = context
     const setColorBlue = () => setColor('blue')
     return { color, setColor, setColorBlue }
 }
